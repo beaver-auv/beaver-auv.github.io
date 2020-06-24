3a8b2db5f9c4b3eada0a7a8c9f47bdbedaa14703
fascettij <44498011+fascettij@users.noreply.github.com>
2020-06-24 15:57:56 -0400

    Update vehicle-script.js

--- vehicle/vehicle-script.js
+++ vehicle/vehicle-script.js
@@ -33,3 +33,34 @@ function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+const appearOptions = {
+  root: null,
+  threshold: 0.25, // how much of content is in view (0-1)
+  rootMargin: "0px 0px -200px 0px" // fade in when item is over 200px in view
+
+};
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const appearOnScroll = new IntersectionObserver(function(entries, appearOnScroll) {
+  entries.forEach(entry => {
+    if (!entry.isIntersecting) {// if false
+      return;
+    } else {
+      entry.target.classList.add('appear');
+      appearOnScroll.unobserve(entry.target);
+    }
+  })
+}, appearOptions);
+
+faders.forEach(fader => {
+  appearOnScroll.observe(fader);
+});
+
+sliders.forEach(slider => {
+  appearOnScroll.observe(slider);
+});
+
